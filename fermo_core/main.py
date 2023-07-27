335bb4ebe29878df2a45469e6c38c777d5f5e3f8
Mitja Zdouc <mitja.zdouc@wur.nl>
2023-07-27 11:53:20 +0200

    Setup of pre-commit<

--- fermo_core/main.py
+++ fermo_core/main.py
@@ -6,21 +6,5 @@ from importlib import metadata
 VERSION = metadata.version("fermo_core")
 
 
-
-def placeholder_function(a, b):
-    """Add two numbers.
-
-    Args:
-        a (int): The first number.
-        b (int): The second number.
-
-    Returns:
-        int: The sum of the two numbers.
-    """
-    return a + b
-
-
-
-
 if __name__ == "__main__":
     print(VERSION)
