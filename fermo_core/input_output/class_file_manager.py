ab94101be8d740806561f01a38ff12ee3e696600
Mitja Zdouc <mitja.zdouc@wur.nl>
2023-11-29 21:23:42 +0100

    Separated reading functions into class 'FileManager' and covered with tests.

---
+++ fermo_core/input_output/class_file_manager.py
@@ -0,0 +1,59 @@
+"""Manages methods for reading and writing files.
+
+Copyright (c) 2022-2023 Mitja Maximilian Zdouc, PhD
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import json
+import logging
+from pathlib import Path
+
+from fermo_core.input_output.class_validation_manager import ValidationManager
+
+
+class FileManager:
+    """Manages exclusively static methods for reading and writing of files."""
+
+    @staticmethod
+    def load_json_file(json_file: str) -> dict:
+        """Validates json file and attempts to load it.
+
+        Parameters:
+            json_file: a filepath to a json file
+
+        Returns:
+            The loaded file as a dict.
+        """
+        try:
+            ValidationManager.validate_file_exists(json_file)
+            ValidationManager.validate_file_extension(json_file, ".json")
+
+            with open(Path(json_file)) as infile:
+                return json.load(infile)
+
+        except json.JSONDecodeError as e:
+            logging.error(f"Error decoding JSON file '{json_file}': wrong format.")
+            raise e
+        except FileNotFoundError as e:
+            logging.error(str(e))
+            raise e
+        except TypeError as e:
+            logging.error(str(e))
+            raise e
