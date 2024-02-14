1e6afb64576ae887ab5326a3907bf1b072f08ecb
Koen1237 <koen.vaningen@wur.nl>
2024-02-14 17:23:54 +0100

    Added method extract_filenames to class, adjusted extract_metadata input

--- mibig_preprocessing/parse_mibig_entries.py
+++ mibig_preprocessing/parse_mibig_entries.py
@@ -1,8 +1,9 @@
 """Processes a folder of all MIBiG entries and outputs 2 space delimited .csv files.
 """
 
-# import pathlib
+from pathlib import Path
 import json
+from sys import argv
 
 # import pandas as pd
 
@@ -16,7 +17,7 @@ class ParseMibigEntries:
         prepped_cfmid_file: Path of output file containing metabolite name, SMILES.
         prepped_metadata_file: Path of output file containing metabolite name, SMILES, chemical formula,
         molecular mass, database IDs, MIBiG entry ID.
-        bgc_file: Path of a .json file containing the information on a single BCG cluster from MIBiG database.
+        bgc_files: A list containing all file paths of the MIBiG entries .json files
         bgc_dict: Dictionary with metabolite_name as key and metadata in a list as values: SMILES, chemical formula,
         molecular mass, database IDs, MIBiG entry ID.
     """
@@ -31,15 +32,15 @@ class ParseMibigEntries:
         self.prepped_cfmid_file = prepped_cfmid_file
         self.prepped_metadata_file = prepped_metadata_file
         # Will later make method to handle file delivery to extract_metadata
-        self.bgc_file = "test.json"
+        self.bgc_files = []
         self.bgc_dict = {}
 
-    def extract_metadata(self):
+    def extract_metadata(self, file_path):
         """Extracts the relevant metadata from a .json file and
         adds a new entry to bgc_dict for every metabolite found.
 
         Attributes:
-            self.bgc_file: Path of a .json file containing the information on a single BCG cluster from MIBiG database.
+            file_path: Path of a .json file containing the information on a single BCG cluster from MIBiG database.
             self.bgc_dict: Dictionary with metabolite_name as key and metadata in a list as values: SMILES,
              chemical formula, molecular mass, database IDs, MIBiG entry ID.
 
@@ -47,7 +48,7 @@ class ParseMibigEntries:
             KeyError: If .json file doesn't contain either a metabolite name or SMILES.
         """
 
-        with open(self.bgc_file) as file:
+        with open(file_path) as file:
             complete_bgc_dict = json.load(file)
             metadata_table = [
                 None,
@@ -60,11 +61,11 @@ class ParseMibigEntries:
             for metabolite in complete_bgc_dict["cluster"]["compounds"]:
                 try:
                     metadata_table[0] = metabolite["compound"]
-                except KeyError():
+                except TypeError():
                     print("No metabolite name found for " + metadata_table[5] + " !")
                 try:
                     metadata_table[1] = metabolite["chem_struct"]
-                except KeyError():
+                except TypeError("No SMILES found for " + metadata_table[5] + " !"):
                     print("No SMILES found for " + metadata_table[5] + " !")
                 metadata_table[2] = metabolite["molecular_formula"]
                 metadata_table[3] = metabolite["mol_mass"]
@@ -77,8 +78,28 @@ class ParseMibigEntries:
                     metadata_table[5],
                 ]
 
+    def extract_filenames(self):
+        """Extracts the filenames of all .json files from a folder and adds them to self.bcg_files
 
-# Testing
-testje = ParseMibigEntries("1", "1", "1")
-testje.extract_metadata()
-print(testje.bgc_dict)
+        Attributes:
+            self.mibig_folder: Path of the mibig.json folder containing .json files.
+            self.bcg_files: A list containing all file paths of the MIBiG entries .json files
+        """
+        folder = Path(self.mibig_folder)
+        for item in folder.iterdir():
+            if item.is_dir():
+                pass
+            else:
+                if item.suffix == ".json":
+                    self.bgc_files.append(str(item))
+
+
+# testing
+if __name__ == "__main__":
+    testje = ParseMibigEntries(argv[1], "1", "1")
+    testje.extract_filenames()
+    for path_file in testje.bgc_files:
+        testje.extract_metadata(path_file)
+    # print(testje.bgc_files)
+    # testje.extract_metadata()
+    print(testje.bgc_dict)
