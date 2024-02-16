01e95c5cf03a743851656b44352748396717c251
Koen1237 <koenvaningen@hotmail.com>
2024-02-16 11:23:46 +0100

    Added copyright notice to header of library_prep.py and parse_mibig_entries.py

--- mibig_preprocessing/library_prep.py
+++ mibig_preprocessing/library_prep.py
@@ -1,4 +1,24 @@
 """Drives the different classes of the MIBiG spectral library pipeline
+
+Copyright (c) 2022 to present Mitja M. Zdouc, PhD and individual contributors.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 
 from parse_mibig_entries import ParseMibigEntries
@@ -41,8 +61,8 @@ class LibraryPrep:
         for file_path in preprocessed_data.bgc_files:
             preprocessed_data.extract_metadata(file_path)
         preprocessed_data.write_outfiles()
-        """ This step should probably take place after CFMID processing
-        preprocessed_data.cleanup_output()"""
+        # This step should probably take place after CFm-ID processing
+        # preprocessed_data.cleanup_output()
 
 
 # Will later do better and flexible input handling using argparse
